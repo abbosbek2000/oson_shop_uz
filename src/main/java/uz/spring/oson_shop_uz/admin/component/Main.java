90d3521305f40380152b0d39f90b99612c8c511b
abbosmamasodirov <mamasodirovabbos@gmail.com>
2022-10-30 11:01:47 +0500

    remove obj

--- src/main/java/uz/spring/oson_shop_uz/admin/component/Main.java
+++ src/main/java/uz/spring/oson_shop_uz/admin/component/Main.java
@@ -2,9 +2,6 @@ package uz.spring.oson_shop_uz.admin.component;
 
 public class Main {
     public static void main(String[] args) throws CloneNotSupportedException {
-        Student student1 = new Student("Abbos", 22);
-        Student student2 = student1.clone();
-        System.out.println(student2.hashCode());
-        System.out.println(student1.hashCode());
+
     }
 }
